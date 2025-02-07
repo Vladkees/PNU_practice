e12b0bd225b4ef1899a411410908b7a72efa689e
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-02-07 14:37:51 +0200

    Update PrintResult.java

--- PNU_FX/src/main/java/com/example/pnu_fx/PrintResult.java
+++ PNU_FX/src/main/java/com/example/pnu_fx/PrintResult.java
@@ -1,54 +1,48 @@
 package com.example.pnu_fx;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 public class PrintResult {
-
     @FXML
     private Label uploadText;
-
-
-
     @FXML
     protected void onUploadButtonClick() {
-        // Створюємо вікно для вибору файлу
         FileChooser fileChooser = new FileChooser();
-
-        // Фільтри для типів файлів (за бажанням)
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files", "*.*"));
         String file1Path = null;
         String file2Path = null;
-        // Отримуємо поточне вікно Stage
         Stage stage = (Stage) uploadText.getScene().getWindow();
-
         File selectedFile1 = fileChooser.showOpenDialog(stage);
         if (selectedFile1 != null) {
-             file1Path = selectedFile1.getAbsolutePath();  // Зберігаємо шлях до першого файлу
+             file1Path = selectedFile1.getAbsolutePath();
             uploadText.setText("Файл 1 вибрано: " + selectedFile1.getName());
         } else {
             uploadText.setText("Файл 1 не вибрано");
-
         }
-
-        // Вибір другого файлу
         File selectedFile2 = fileChooser.showOpenDialog(stage);
         if (selectedFile2 != null) {
-            file2Path = selectedFile2.getAbsolutePath();  // Зберігаємо шлях до другого файлу
+            file2Path = selectedFile2.getAbsolutePath();
             uploadText.setText(uploadText.getText() + "\nФайл 2 вибрано: " + selectedFile2.getName());
         } else {
             uploadText.setText(uploadText.getText() + "\nФайл 2 не вибрано");
-
         }
         if (file1Path != null && file2Path != null) {
             LevensteinDistance levenstein = new LevensteinDistance();
             String firstWord = levenstein.readFileContent(file1Path);
             String secondWord = levenstein.readFileContent(file2Path);
-            int distance = levenstein.calculation(firstWord, secondWord);
+            // Отримання списку змін
+            List<LevensteinDistance.EditOperation> differences = levenstein.findDifferences(firstWord, secondWord);
+            // Кількість змін - це фактична відстань Левенштейна
+            int distance = differences.size();
             uploadText.setText(uploadText.getText() + "\nВідстань Левенштейна: " + distance);
+
+            for (LevensteinDistance.EditOperation edit : differences) {
+                uploadText.setText(uploadText.getText() + "\n" + edit);
         }
     }
-}
+}}
